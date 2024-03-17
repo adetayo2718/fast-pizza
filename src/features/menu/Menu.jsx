1c12bd5268e41d752ed3f06932cd69d9e99715cb
adetayo2718 <adetayo2718@gmail.com>
2024-03-17 20:29:20 +0100

    Updated

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,23 +1,22 @@
-import { useLoaderData, useNavigation } from "react-router-dom";
-import { getMenu } from "../../service/apiRestaurant";
-import MenuItem from "./MenuItem";
-import Loader from "../../ui/Loader";
-
-export function loader() {
-  const data = getMenu();
-  return data;
-}
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 
 function Menu() {
   const menu = useLoaderData();
 
   return (
-    <ul>
+    <ul className="divide-y divide-stone-200 px-2">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
     </ul>
   );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
